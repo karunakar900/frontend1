e1ace502ade30f39968f2ea42169db01037eddfd
karunakar900 <karunakar.koyada234@gmail.com>
2023-04-05 12:12:45 +0530

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Logincomponent from './components/Logincomponent';
+import Signupcomponent from './components/Signupcomponent';
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Logincomponent />} />
+                <Route path="/register" element={<Signupcomponent />} />
+
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default App;
---
+++ src/components/todo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Todo = () => {
+    return (
+        <div>
+            <p>welcome to your dashboard</p>
+        </div>
+    )
+}
+
+export default Todo;
